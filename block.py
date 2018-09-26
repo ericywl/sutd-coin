04c8c97aff6cde854a21d73a7d3a1e88ddebddf1
ericywl <ericywl@live.com>
2018-09-27 00:18:37 +0800

    Update block with validation

--- block.py
+++ block.py
@@ -26,6 +26,52 @@ class Block:
                 return cls(header, transactions)
             header["nonce"] += 1
 
+    def to_json(self):
+        return json.dumps({
+            "header": self._header,
+            "transactions": self._transactions
+        })
+
+    @classmethod
+    def from_json(cls, json_str):
+        obj = json.loads(json_str)
+        fields = ["header", "transactions"]
+        for f in fields:
+            if not obj[f]:
+                raise Exception("Block JSON string is invalid.")
+        header_fields = ["prev_hash", "root", "timestamp", "nonce"]
+        for f in header_fields:
+            if not obj["header"][f]:
+                raise Exception("Block JSON string is invalid.")
+        block = cls(obj["header"], obj["transactions"])
+        if block.validate():
+            return block
+
+    def validate(self):
+        # Validate block
+        ## Check block header type
+        if type(self._header) != dict:
+            raise Exception("Block header not dictionary.")
+        ## Check previous header hash
+        if type(self._header["prev_hash"]) != str:
+            raise Exception("Previous header hash not string.")
+        if len(self._header["prev_hash"]) != str:
+            raise Exception("Previous header hash length is invalid.")
+        ## Check timestamp
+        if type(self._header["timestamp"]) != int:
+            raise Exception("Timestamp not integer.")
+        if self._header["timestamp"] <= 0:
+            raise Exception("Invalid timestamp value.")
+        ## Check nonce
+        if type(self._header["nonce"]) != int:
+            raise Exception("Nonce not integer.")
+        if self._header["nonce"] <= 0:
+            raise Exception("Invalid nonce value.")
+        ## Check transactions
+        if type(self._transactions) != list:
+            raise Exception("Transactions not list.")
+        return True
+
     @property
     def transactions(self):
         return self._transactions
@@ -33,5 +79,10 @@ class Block:
     @property
     def header(self):
         return self._header
+
+if __name__ == "__main__":
+    b = Block.new("abcd")
+    j = json.dumps(b)
+    print(j)
 
 
