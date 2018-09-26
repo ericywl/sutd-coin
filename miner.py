654e0e1a67d8349d38794f5d41631eadb23395c0
ericywl <midnight2903@gmail.com>
2018-09-26 21:44:34 +0800

    Update miner

--- miner.py
+++ miner.py
@@ -4,10 +4,11 @@ from blockchain import *
 
 class Miner:
     def __init__(self, privkey, pubkey):
-        self.privkey = privkey
-        self.pubkey = pubkey
-        self.blockchain = None
-        self.transactions = set()
+        self._privkey = privkey
+        self._pubkey = pubkey
+        self._blockchain = None
+        self._transactions = set()
+        self._peers = []
 
     @classmethod
     def new(cls):
@@ -15,8 +16,17 @@ class Miner:
         pubkey = privkey.get_verifying_key()
         return cls(privkey, pubkey)
 
-    def create_transaction():
-        return
+    def _broadcast_transaction(self, trans_json):
+        # assume that peers are all nodes in the network
+        # (of course, not practical IRL since its not scalable)
+        for p in self._peers:
+            p.add_transaction(trans_json)
+
+    def create_transaction(self, receiver, amount, comment):
+        trans = Transaction.new(self.pubkey, receiver, amount, comment)
+        trans_json = trans.to_json()
+        self.add_transaction(trans_json)
+        self._broadcast_transaction(trans_json)
 
     def add_transaction(self, trans_json):
         trans = Transaction.from_json(trans_json)
@@ -25,7 +35,9 @@ class Miner:
         else:
             self.transactions.add(trans_json)
 
-    def create_block():
+    def create_block(self):
+        self.blockchain.resolve()
+        added_trans =
         return
 
     def add_block():
