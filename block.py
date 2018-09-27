f76c8e9ee93366135d806108f0a6ed721c73375f
ericywl <midnight2903@gmail.com>
2018-09-27 23:29:49 +0800

    Update block with verification

--- block.py
+++ block.py
@@ -1,7 +1,8 @@
 from merkle_tree import *
+from transaction import *
 import algo
 
-import datetime, json
+import datetime, json, copy
 
 class Block:
     _zeroes = "000"
@@ -36,41 +37,66 @@ class Block:
     def from_json(cls, json_str):
         obj = json.loads(json_str)
         fields = ["header", "transactions"]
-        for f in fields:
-            if not obj[f]:
-                raise Exception("Block JSON string is invalid.")
+        if all(elem in obj.values() for elem in fields):
+            raise Exception("Block JSON string is invalid.")
         header_fields = ["prev_hash", "root", "timestamp", "nonce"]
-        for f in header_fields:
-            if not obj["header"][f]:
-                raise Exception("Block JSON string is invalid.")
+        if all(elem in obj["header"].values() for elem in header_fields):
+            raise Exception("Block JSON header is invalid.")
         block = cls(obj["header"], obj["transactions"])
         if block.validate():
             return block
 
+    # Validate block
     def validate(self):
-        # Validate block
-        ## Check block header type
+        # Check block header type
         if type(self._header) != dict:
             raise Exception("Block header not dictionary.")
-        ## Check previous header hash
+        # Check previous header hash
         if type(self._header["prev_hash"]) != str:
             raise Exception("Previous header hash not string.")
-        if len(self._header["prev_hash"]) != str:
+        if len(self._header["prev_hash"]) != algo.HASH_LEN:
             raise Exception("Previous header hash length is invalid.")
-        ## Check timestamp
-        if type(self._header["timestamp"]) != int:
-            raise Exception("Timestamp not integer.")
+        # Check timestamp
+        if type(self._header["timestamp"]) != float:
+            raise Exception("Timestamp not float.")
         if self._header["timestamp"] <= 0:
             raise Exception("Invalid timestamp value.")
-        ## Check nonce
+        # Check nonce
         if type(self._header["nonce"]) != int:
             raise Exception("Nonce not integer.")
-        if self._header["nonce"] <= 0:
-            raise Exception("Invalid nonce value.")
-        ## Check transactions
+        if self._header["nonce"] < 0:
+            raise Exception("Nonce cannot be negative.")
+        # Check transactions
         if type(self._transactions) != list:
             raise Exception("Transactions not list.")
         return True
+
+    # Compare calculated root with stored root
+    def _check_root(self):
+        calc_root = MerkleTree(self._transactions).get_root()
+        return calc_root == self._header["root"]
+
+    # Verify all transactions
+    def _verify_transactions(self):
+        for t_json in self._transactions:
+            t = Transaction.from_json(t_json)
+            if not t.verify():
+                return False
+        return True
+
+    def verify(self):
+        # Check Merkle Tree root of block
+        if not self._check_root():
+            raise Exception("Invalid root in block.")
+        # Verify transactions in block
+        if not self._verify_transactions():
+            raise Exception("Some transactions are invalid.")
+        return True
+
+    def __eq__(self, other):
+        j1 = self.to_json()
+        j2 = other.to_json()
+        return j1 == j2
 
     @property
     def transactions(self):
@@ -81,8 +107,9 @@ class Block:
         return self._header
 
 if __name__ == "__main__":
-    b = Block.new("abcd")
-    j = json.dumps(b)
-    print(j)
+    import os
+    b1 = Block.new(os.urandom(32).hex())
+    b2 = Block.from_json(b1.to_json())
+    print(b1 == b2)
 
 
