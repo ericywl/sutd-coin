6d795bee3f9f5d1789c9161ff743eea084d53f75
ericywl <midnight2903@gmail.com>
2018-09-27 23:40:24 +0800

    Update blockchain add-block-verification

--- blockchain.py
+++ blockchain.py
@@ -18,8 +18,8 @@ class Blockchain:
         endhash_clen_map = { genesis_hash: 0 }
         return cls(hash_block_map, endhash_clen_map)
 
+    # Compute chain length from block (not including genesis)
     def _get_chain_length(self, block):
-        # Compute chain length from block
         prev_hash = block.header["prev_hash"]
         chain_len = 0
         while prev_hash != None:
@@ -30,11 +30,10 @@ class Blockchain:
                     break
         return chain_len
 
+    # Add new block to chain
     def add(self, block):
-        # Add new block to chain 
-        # Validate block 
-        if not self.validate(block):
-            raise Exception("Invalid block.")
+        # Verify block
+        self.verify(block)
         curr_hash = algo.hash2_dic(block.header)
         prev_hash = block.header["prev_hash"]
         self._hash_block_map[curr_hash] = block
@@ -50,15 +49,15 @@ class Blockchain:
         else:
             self._endhash_clen_map[curr_hash] = self._get_chain_length(block)
 
-    def _prev_exist(self, prev_hash):
-        # Check block with prev_hash exist in list
+    # Check block with prev_hash exist in list
+    def _check_prev_exist(self, prev_hash):
         for b in self._hash_block_map.values():
             if prev_hash == algo.hash2_dic(b.header):
                 return True
         return False
 
+    # Check timestamp larger than median of previous 11 timestamps
     def _check_timestamp(self, block):
-        # Check timestamp larger than median of previous 11 timestamps
         prev_timestamps = []
         prev_hash = block.header["prev_hash"]
         while prev_hash != None and len(prev_timestamps) < 11:
@@ -67,26 +66,37 @@ class Blockchain:
             prev_hash = b.header["prev_hash"]
         return block.header["timestamp"] > statistics.median(prev_timestamps)
 
-    def validate(self, block):
-        # Validate the block
-        # Check header hash matches and is smaller than Block.TARGET
+    # Check if block contains transactions that are already in chain
+    def _check_duplicate_trans(self, blk_transactions):
+        trans_set = set(self.transactions)
+        blk_trans_set = set(blk_transactions)
+        num_b_transactions = len(blk_trans_set)
+        remaining_transactions = blk_trans_set - trans_set
+        return len(remaining_transactions) == num_b_transactions
+
+    # Verify the block
+    def verify(self, block):
+        # Verify and validate block (self-contained)
+        block.validate()
+        block.verify()
+        # Verify the block in the context of the blockchain
+        ## Check header hash matches and is smaller than Block.TARGET
         comp_hash = algo.hash2_dic(block.header)
         if comp_hash >= Block.TARGET:
-            print("Error: Invalid header hash in block")
-            return False
-        prev_hash = block.header["prev_hash"]
-        # Check previous block exist in blocks list
-        if not self._prev_exist(prev_hash):
-            print("Error: Previous block does not exist.")
-            return False
-        # Check timestamp validity
+            raise Exception("Invalid header hash in block")
+        ## Check previous block exist in blocks list
+        if not self._check_prev_exist(block.header["prev_hash"]):
+            raise Exception("Previous block does not exist.")
+        ## Check transactions in blockchain not reused in block
+        if not self._check_duplicate_trans(block.transactions):
+            raise Exception("Duplicate transaction found in blockchain.")
+        ## Check timestamp in block header
         if not self._check_timestamp(block):
-            print("Error: Invalid timestamp in block.")
-            return False
+            raise Exception("Invalid timestamp in block.")
         return True
 
+    # Compute proof of work of chain from last block
     def _get_chain_pow(self, block):
-        # Compute chain length from block
         prev_hash = block.header["prev_hash"]
         chain_pow = block.header["nonce"]
         while prev_hash != None:
@@ -97,11 +107,12 @@ class Blockchain:
                     break
         return chain_pow
 
+    # Convenience function for lambda in resolve()
     def _pow(block_hash):
         return self._get_chain_pow(self._hash_block_map[block_hash])
 
+    # Resolve potential forks in block and return last block
     def resolve(self):
-        # Resolve potential forks in block
         # No forks
         if len(self._endhash_clen_map) == 1:
             blk_hash = list(self._endhash_clen_map.keys())[0]
@@ -111,7 +122,7 @@ class Blockchain:
         block_hashes = [
             k for k, v in self._endhash_clen_map.items() if v == longest_clen
         ]
-        # Multiple chain with same length exist, 
+        # Multiple chain with same length exist,
         # use PoW ie. nonce to determine which to keep
         if len(block_hashes) != 1:
             block_hashes = [ max(block_hashes, key=lambda bh: self._pow(bh)) ]
@@ -130,17 +141,14 @@ class Blockchain:
 
     @property
     def blocks(self):
-        res = []
-        for b in list(self._hash_block_map.values()):
-            res.append(copy.deepcopy(b))
-        return res
+        return list(self._hash_block_map.values())
 
     @property
     def transactions(self):
         res = []
         for b in self.blocks:
             for t in b.transactions:
-                res.append(copy.deepcopy(t))
+                res.append(t)
         return res
 
     @property
@@ -152,28 +160,36 @@ class Blockchain:
         return self._endhash_clen_map
 
 
+def generate_transactions(n):
+    transactions = []
+    for j in range(n):
+        sender_sk = ecdsa.SigningKey.generate()
+        sender_vk = sender_sk.get_verifying_key()
+        receiver_sk = ecdsa.SigningKey.generate()
+        receiver_vk = receiver_sk.get_verifying_key()
+        t = Transaction.new(sender_vk, receiver_vk, 1, sender_sk, j, str(j))
+        transactions.append(t.to_json())
+    return transactions
+
 def main():
     blockchain = Blockchain.new()
     hashes = []
+    # Generate 10 blocks with 10 transactions per block
     for i in range(10):
-        transactions = []
-        for j in range(10):
-            sender_sk = ecdsa.SigningKey.generate()
-            sender_vk = sender_sk.get_verifying_key()
-            receiver_sk = ecdsa.SigningKey.generate()
-            receiver_vk = receiver_sk.get_verifying_key()
-            t = Transaction.new(sender_vk, receiver_vk, j+1, sender_sk, str(j))
-            transactions.append(t.to_json())
+        transactions = generate_transactions(10)
         prev_block = blockchain.resolve()
         prev_hash = algo.hash2_dic(prev_block.header)
         new_block = Block.new(prev_hash, transactions)
         hashes.append(algo.hash2_dic(new_block.header))
         blockchain.add(new_block)
+    # Introduce fork
     prev_hash = hashes[2]
     for i in range(4):
+        transactions = generate_transactions(10)
         fork_block = Block.new(prev_hash, transactions)
         blockchain.add(fork_block)
         prev_hash = algo.hash2_dic(fork_block.header)
+    # Try to resolve
     print("Pre-resolve: " + str(blockchain.endhash_clen_map))
     last_blk = blockchain.resolve()
     print("Post-resolve: " + str(blockchain.endhash_clen_map))
